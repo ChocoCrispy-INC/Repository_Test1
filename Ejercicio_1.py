0b554a7ad62ff3e086b147f4635282cf6adcfd1d
ChocoCrispy - INC <113120515+ChocoCrispy-INC@users.noreply.github.com>
2022-09-14 00:30:57 -0500

    Update Ejercicio_1.py

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,7 +1,6 @@
 print("Hola Git Mode Python")
 #System.out.println("Hola Git Mode Java");
-
 print("Mi número favorito es el 4")
 print("I LOVE ANIME")
 print("My favorite food is sushi")
-print("No se que poner :V")
+print("No se que más poner :V")
