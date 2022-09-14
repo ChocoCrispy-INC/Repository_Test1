7111edd5d1aa048f1e71807ad5b0b271882a36f1
Crispy <chococrispy.inc@gmail.com>
2022-09-14 18:04:51 -0500

    Realizando otra rama con solo un archivo

---
+++ Ejercicio3.py
@@ -0,0 +1,8 @@
+print("Hola Git Mode Python")
+System.out.println("Hola Git Mode Java");
+print("Mi número favorito es el 4")
+print("I LOVE ANIME")
+print("My favorite food is sushi")
+print("My favorite singer is Dimash Qudaibergen")
+print("No tengo color favorito..aun")
+print("No se que más poner :V ")
