cd33f30a0e64ef59f6c4dc9c4af6c894fd13129b
Crispy <chococrispy.inc@gmail.com>
2022-09-14 00:52:19 -0500

    Nuevo comment

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -1,7 +1,7 @@
 print("Hola Git Mode Python")
-#System.out.println("Hola Git Mode Java");
-
+System.out.println("Hola Git Mode Java");
 print("Mi número favorito es el 4")
 print("I LOVE ANIME")
 print("My favorite food is sushi")
-print("No se que poner :V")
+print("My favorite singer is Dimash Qudaibergen")
+print("No se que más poner :V")
